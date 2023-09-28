86339b8e7dc69ae364fa8cbdce7013d2885f4a58
Kalaiyarasan A <kalaiyarasan.a@rfpio.com>
2023-09-28 11:29:36 +0530

    script commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const data = [
+      { firstName: "John", lastName: "Smath" },
+      { firstName: "Paul", lastName: "Smeth" },
+      { firstName: "Cody", lastName: "Smith" },
+      { firstName: "Jordan", lastName: "Smoth" },
+      { firstName: "Jim", lastName: "Smuth" },
+    ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        data.map((user) => {
+          return <div>
+            <p>{user.firstName} {user.lastName}</p>
+          </div>
+        })
+      }
     </div>
   );
 }
